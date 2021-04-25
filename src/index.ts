bc93b2716d9a5fa6a20e38774a3be46a37869e49
Ismaeel <ismaeelakram01@gmail.com>
2021-04-25 19:03:23 -0400

    Add express server for heroku

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 require('dotenv').config();
+import express from 'express';
+
 import * as log from './log';
 import Bot from './common/bot';
 import AuthObject from './common/auth';
+import { ppid } from 'node:process';
 
+const app = express();
 let auth: AuthObject = {
 	username: process.env.username || '',
 	OAuthToken: process.env.OAuthToken || '',
@@ -12,4 +16,12 @@ let auth: AuthObject = {
 export let bot = new Bot(auth, ['mahjestic']);
 bot.start().catch((err) => {
 	log.danger('Error starting bot: ' + err);
+});
+
+app.get('/', (req: express.Request, res: express.Response): void => {
+	res.send('Hey');
+});
+
+app.listen(80, () => {
+	log.good('Express server started');
 });
