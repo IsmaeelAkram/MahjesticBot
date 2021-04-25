8ea61e59460846319b9f1c6e9e79850aceeec004
Ismaeel <ismaeelakram01@gmail.com>
2021-04-25 03:25:26 -0400

    Switch bot from interface to class

--- src/common/bot.ts
+++ src/common/bot.ts
@@ -1,7 +1,37 @@
-import { AuthObject } from './auth';
+import AuthObject from './auth';
+import * as log from '../log';
+
 import tmi from 'tmi.js';
 
-interface bot {
-	auth: AuthObject;
-	client: tmi.Client;
+export default class Bot {
+	public readonly auth: AuthObject;
+	public client: tmi.Client;
+	public channels: string[];
+
+	constructor(auth: AuthObject, channels: string[]) {
+		this.auth = auth;
+		this.channels = channels;
+		this.client = new tmi.Client({
+			connection: { reconnect: true },
+			identity: {
+				username: auth.username,
+				password: auth.OAuthToken,
+			},
+			channels,
+		});
+	}
+
+	async registerEvents(): Promise<void> {
+		await this.client.on('join', (channel: string, username: string) => {
+			log.good(`Joined channel '${channel}'`);
+		});
+	}
+
+	async start(): Promise<void> {
+		log.info('Registering events');
+		await this.registerEvents();
+		log.info('Starting bot');
+		await this.client.connect();
+		log.good('Started bot');
+	}
 }
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
 import * as log from './log';
+import Bot from './common/bot';
+import AuthObject from './common/auth';
+
+import config from '../config.json';
+
+let auth: AuthObject = {
+	username: config.username,
+	OAuthToken: config.OAuthToken,
+	clientId: config.clientId,
+};
+let bot = new Bot(auth, ['mahjestic']);
+bot.start();
