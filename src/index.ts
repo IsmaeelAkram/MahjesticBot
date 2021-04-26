9ae90fd7ae37c1fb998dd66be51e92832eaf895d
Ismaeel <ismaeelakram01@gmail.com>
2021-04-26 18:43:51 -0400

    Don’t log auth

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,14 @@ require('dotenv').config();
 import * as log from './log';
 import Bot from './common/bot';
 import AuthObject from './common/auth';
+
 let auth: AuthObject = {
 	username: process.env.username || '',
 	OAuthToken: process.env.oauth_token || '',
 	clientId: process.env.client_id || '',
 };
-console.log(auth);
-
 export let bot = new Bot(auth, ['mahjestic']);
+
 bot.start().catch((err) => {
 	log.danger('Error starting bot: ' + err);
 });
