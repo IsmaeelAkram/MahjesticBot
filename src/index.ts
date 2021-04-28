44ae3139722628a9db54109d8200d36ba76427d7
Ismaeel <ismaeelakram01@gmail.com>
2021-04-28 02:17:01 -0400

    Glitch to heroku

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,7 @@ require('dotenv').config();
 import * as log from './log';
 import Bot from './common/bot';
 import AuthObject from './common/auth';
-import express, { Request, Response } from 'express';
 
-const app = express();
-const PORT = process.env.PORT || 80;
 const auth: AuthObject = {
 	username: process.env.username || '',
 	OAuthToken: process.env.oauth_token || '',
@@ -17,13 +14,4 @@ export let bot = new Bot(auth, ['mahjestic']);
 
 bot.start().catch((err) => {
 	log.danger('Error starting bot: ' + err);
-});
-
-app.get('/', (req: Request, res: Response) => {
-	log.info(`GET / ${req.headers['x-forwarded-for']?.valueOf() || 'Unknown IP'} 200`);
-	res.sendStatus(200);
-});
-
-app.listen(PORT, () => {
-	log.good('Express Server started');
 });
